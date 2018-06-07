2ca409488d5fd3c184debea15d10e6e29f6f241c
James E Keenan <jkeenan@cpan.org>
2018-06-07 15:19:16 -0400

    Initial file and directory structure generated by modulemaker.

---
+++ lib/CPAN/Cpanorg/Auxiliary.pm
@@ -0,0 +1,80 @@
+package CPAN::Cpanorg::Auxiliary;
+use strict;
+
+BEGIN {
+    use Exporter ();
+    use vars qw($VERSION @ISA @EXPORT @EXPORT_OK %EXPORT_TAGS);
+    $VERSION     = '0.01';
+    @ISA         = qw(Exporter);
+    #Give a hoot don't pollute, do not export more than needed by default
+    @EXPORT      = qw();
+    @EXPORT_OK   = qw();
+    %EXPORT_TAGS = ();
+}
+
+
+#################### main pod documentation begin ###################
+## Below is the stub of documentation for your module.
+## You better edit it!
+
+
+=head1 NAME
+
+CPAN::Cpanorg::Auxiliary - Functions used in cpan.org infrastructure
+
+=head1 SYNOPSIS
+
+  use CPAN::Cpanorg::Auxiliary;
+  blah blah blah
+
+
+=head1 DESCRIPTION
+
+Stub documentation for this module was created by ExtUtils::ModuleMaker.
+It looks like the author of the extension was negligent enough
+to leave the stub unedited.
+
+Blah blah blah.
+
+
+=head1 USAGE
+
+
+
+=head1 BUGS
+
+
+
+=head1 SUPPORT
+
+
+
+=head1 AUTHOR
+
+    James E Keenan
+    CPAN ID: MODAUTHOR
+    Perl 5 Porters
+    jkeenan@cpan.org
+    http://a.galaxy.far.far.away/modules
+
+=head1 COPYRIGHT
+
+This program is free software; you can redistribute
+it and/or modify it under the same terms as Perl itself.
+
+The full text of the license can be found in the
+LICENSE file included with this module.
+
+
+=head1 SEE ALSO
+
+perl(1).
+
+=cut
+
+#################### main pod documentation end ###################
+
+
+1;
+# The preceding line will help the module return a true value
+
