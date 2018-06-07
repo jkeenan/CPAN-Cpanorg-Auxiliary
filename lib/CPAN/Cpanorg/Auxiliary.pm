f50be3f35f4b40327edd594696c7b331ebbd182b
James E Keenan <jkeenan@cpan.org>
2018-06-07 15:24:10 -0400

    Import from cpanorg-generators/bin/perl-sorter.pl
    4 subroutine declarations and 1 variable assignment

--- lib/CPAN/Cpanorg/Auxiliary.pm
+++ lib/CPAN/Cpanorg/Auxiliary.pm
@@ -1,80 +1,117 @@
 package CPAN::Cpanorg::Auxiliary;
-use strict;
+use 5.14.0;
+use warnings;
+use parent 'Exporter';
+our $VERSION = '0.01';
+our @EXPORT_OK = qw(
+);
+use Carp qw/confess/;
+use File::Basename qw/dirname basename/;
+use File::Slurp 9999.19;
+use Getopt::Long;
+use JSON ();
+use LWP::Simple qw(get);
 
-BEGIN {
-    use Exporter ();
-    use vars qw($VERSION @ISA @EXPORT @EXPORT_OK %EXPORT_TAGS);
-    $VERSION     = '0.01';
-    @ISA         = qw(Exporter);
-    #Give a hoot don't pollute, do not export more than needed by default
-    @EXPORT      = qw();
-    @EXPORT_OK   = qw();
-    %EXPORT_TAGS = ();
+my $json = JSON->new->pretty(1);
+
+sub print_file {
+    my ( $file, $data ) = @_;
+
+    write_file( "data/$file", { binmode => ':utf8' }, $data )
+        or die "Could not open data/$file: $!";
 }
 
+sub sort_versions {
+    my $list = shift;
 
-#################### main pod documentation begin ###################
-## Below is the stub of documentation for your module.
-## You better edit it!
+    my @sorted = sort {
+               $b->{version_major} <=> $a->{version_major}
+            || int( $b->{version_minor} ) <=> int( $a->{version_minor} )
+            || $b->{version_iota} <=> $a->{version_iota}
+    } @{$list};
 
+    return \@sorted;
 
-=head1 NAME
+}
 
-CPAN::Cpanorg::Auxiliary - Functions used in cpan.org infrastructure
+sub extract_first_per_version_in_list {
+    my $versions = shift;
 
-=head1 SYNOPSIS
+    my $lookup = {};
+    foreach my $version ( @{$versions} ) {
+        my $minor_version = $version->{version_major} . '.'
+            . int( $version->{version_minor} );
 
-  use CPAN::Cpanorg::Auxiliary;
-  blah blah blah
+        $lookup->{$minor_version} = $version
+            unless $lookup->{$minor_version};
+    }
+    return $lookup;
+}
 
+sub fetch_perl_version_data {
+    my $perl_dist_url = "http://search.cpan.org/api/dist/perl";
 
-=head1 DESCRIPTION
+    my $filename = 'perl_version_all.json';
 
-Stub documentation for this module was created by ExtUtils::ModuleMaker.
-It looks like the author of the extension was negligent enough
-to leave the stub unedited.
+    # See what we have on disk
+    my $disk_json = '';
+    $disk_json = read_file("data/$filename")
+        if -r "data/$filename";
 
-Blah blah blah.
+    my $cpan_json = get($perl_dist_url);
+    die "Unable to fetch $perl_dist_url" unless $cpan_json;
 
+    if ( $cpan_json eq $disk_json ) {
 
-=head1 USAGE
+        # Data has not changed so don't need to do anything
+        exit;
+    } else {
 
+        # Save for next fetch
+        print_file( $filename, $cpan_json );
+    }
 
+    my $data = $json->decode($cpan_json);
 
-=head1 BUGS
+    my @perls;
+    my @testing;
+    foreach my $module ( @{ $data->{releases} } ) {
+        next unless $module->{authorized} eq 'true';
 
+        my $version = $module->{version};
 
+        $version =~ s/-(?:RC|TRIAL)\d+$//;
+        $module->{version_number} = $version;
 
-=head1 SUPPORT
+        my ( $major, $minor, $iota ) = split( '[\._]', $version );
+        $module->{version_major} = $major;
+        $module->{version_minor} = int($minor);
+        $module->{version_iota}  = int( $iota || '0' );
 
+        $module->{type}
+            = $module->{status} eq 'testing'
+            ? 'Devel'
+            : 'Maint';
 
+        # TODO: Ask - please add some validation logic here
+        # so that on live it checks this exists
+        my $zip_file = $module->{distvname} . '.tar.gz';
 
-=head1 AUTHOR
+        $module->{zip_file} = $zip_file;
+        $module->{url} = "http://www.cpan.org/src/5.0/" . $module->{zip_file};
 
-    James E Keenan
-    CPAN ID: MODAUTHOR
-    Perl 5 Porters
-    jkeenan@cpan.org
-    http://a.galaxy.far.far.away/modules
+        ( $module->{released_date}, $module->{released_time} )
+            = split( 'T', $module->{released} );
 
-=head1 COPYRIGHT
+        next if $major < 5;
 
-This program is free software; you can redistribute
-it and/or modify it under the same terms as Perl itself.
+        if ( $module->{status} eq 'stable' ) {
+            push @perls, $module;
+        } else {
+            push @testing, $module;
+        }
+    }
+    return \@perls, \@testing;
+}
 
-The full text of the license can be found in the
-LICENSE file included with this module.
-
-
-=head1 SEE ALSO
-
-perl(1).
-
-=cut
-
-#################### main pod documentation end ###################
-
-
-1;
-# The preceding line will help the module return a true value
 
