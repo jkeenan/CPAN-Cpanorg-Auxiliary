cb902cfe8e41f5d0df2f11f8527deb6f9a0e4ff5
James E Keenan <jkeenan@cpan.org>
2018-10-01 22:07:53 -0400

    CPAN version 0.02
    Update $VERSION.  Add .version_info.pl and MANIFEST.SKIP

---
+++ .version_info.pl
@@ -0,0 +1,8 @@
+%trans = (
+    version => { old => '0.01', new => '0.02' },
+    date    => { old => 'June 24 2018', new => 'October 1 2018' },
+    copyright => { old => '2018', new => '2018' },
+);
+
+1;
+
